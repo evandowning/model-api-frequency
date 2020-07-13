3fe28b8a494de19d113dc2dcd05411f2a6b2bd59
Evan Downing <2077950+evandowning@users.noreply.github.com>
2020-07-13 17:03:17 +0000

    update sklearn version and joblib import

--- api_frequency.py
+++ api_frequency.py
@@ -19,7 +19,8 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
-from sklearn.externals import joblib
+
+import joblib
 
 def _main():
     # Parse arguments
